de87d115978b12e5c326b53acbb672c7f31f808c
tiangengle <tgl1658676840@163.com>
2020-06-18 16:11:33 +0800

    spring cloud bus

--- config-center/config-client/src/main/java/com/tgl/configclient/ConfigClientApplication.java
+++ config-center/config-client/src/main/java/com/tgl/configclient/ConfigClientApplication.java
@@ -2,8 +2,12 @@ package com.tgl.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
